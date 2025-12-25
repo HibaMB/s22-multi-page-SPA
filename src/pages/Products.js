f06390cae7acf28c8b086f250ef9b86bb00b019c
HibaMB <hibabanat.95@gmail.com>
2025-12-25 13:51:48 +0300

    handle params in router, useParams to read the param value

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,9 +1,13 @@
 function ProductsPage() {
   return (
-    <div>
+    <>
       <h1>Products Page</h1>
-      <p>Welcome to the Products Page!</p>
-    </div>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
   );
 }
 
